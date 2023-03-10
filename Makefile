9b10d4567064fc8144be4fe7e36977c31ee12369
Lindon ALIU <lindon.aliu@epitech.eu>
2023-03-10 04:41:10 +0100

    feat: add the print of first line.

--- Makefile
+++ Makefile
@@ -14,6 +14,7 @@ SRCS		=	$(addprefix src/,		\
 					ErrorHandling		\
 					Rules				\
 					Conf				\
+					Wolfram				\
 					Main))				\
 
 OBJS		= $(SRCS:.hs=.o)
--- src/Main.hs
+++ src/Main.hs
@@ -12,18 +12,16 @@ import System.Environment (getArgs)
 import System.Exit (exitWith, ExitCode (ExitFailure))
 
 -- import local modules
-import Rules (rule30, rule90, rule110)
 import Conf (Conf(..), defaultConf, getOpts)
 import ErrorHandling (errorHandling)
-
-wolfram :: Conf -> IO ()
-wolfram _ = putStrLn "Hello World"
+import Rules (rule30, rule90, rule110)
+import Wolfram (wolfram)
 
 main :: IO ()
 main = getArgs >>= arguments
-  where arguments x | null x = exitWith (ExitFailure 84)
-        arguments (x:xs) | not (errorHandling (x:xs)) =
-                            exitWith (ExitFailure 84)
-        arguments (x:xs) = case getOpts defaultConf (x:xs) of
-          Just conf -> wolfram conf
-          Nothing -> exitWith (ExitFailure 84)
+  where arguments x       | null x = exitWith (ExitFailure 84)
+        arguments (x:xs)  | not (errorHandling (x:xs)) =
+                              exitWith (ExitFailure 84)
+        arguments (x:xs)  = case getOpts defaultConf (x:xs) of
+                              Just conf -> wolfram conf
+                              Nothing -> exitWith (ExitFailure 84)
