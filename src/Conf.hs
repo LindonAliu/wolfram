3a01c3fd97d5df0b805874a687a98e293dd9cb4c
Lindon ALIU <lindon.aliu@epitech.eu>
2023-03-11 19:11:20 +0100

    fix: correctin of structure conf with Maybe deleting

--- src/Conf.hs
+++ src/Conf.hs
@@ -7,30 +7,33 @@
 
 module Conf (Conf(..), defaultConf, getOpts) where
 
-data Conf = Conf { rule :: Maybe Int,
-                   start :: Int,
-                   line :: Maybe Int,
-                   window :: Int,
-                   move :: Maybe Int
-                 } deriving (Show)
+data Conf = Conf    {
+                        rule :: Int,
+                        start :: Int,
+                        line :: Int,
+                        window :: Int,
+                        move :: Int
+                    } deriving (Show)
 
 defaultConf :: Conf
-defaultConf = Conf { rule = Nothing,
-                     start = 0,
-                     line = Nothing,
-                     window = 80,
-                     move = Nothing }
+defaultConf = Conf  {
+                        rule = -1,
+                        start = 0,
+                        line = -1,
+                        window = 80,
+                        move = 0
+                    }
 
 getOpts :: Conf -> [String] -> Maybe Conf
-getOpts conf [] = Just conf
-getOpts conf ("--rule":xs) = getOpts (conf {
-                             rule = Just (read (head xs) :: Int) }) (tail xs)
-getOpts conf ("--start":xs) = getOpts (conf {
-                              start = read (head xs) :: Int }) (tail xs)
-getOpts conf ("--lines":xs) = getOpts (conf {
-                              line = Just (read (head xs) :: Int) }) (tail xs)
-getOpts conf ("--window":xs) = getOpts (conf {
-                               window = read (head xs) :: Int }) (tail xs)
-getOpts conf ("--move":xs) = getOpts (conf {
-                             move = Just (read (head xs) :: Int) }) (tail xs)
-getOpts _ _ = Nothing
+getOpts conf []                 = Just conf
+getOpts conf ("--rule":xs)      = getOpts (conf {
+                                    rule = (read (head xs) :: Int) }) (tail xs)
+getOpts conf ("--start":xs)     = getOpts (conf {
+                                    start = read (head xs) :: Int }) (tail xs)
+getOpts conf ("--lines":xs)     = getOpts (conf {
+                                    line = (read (head xs) :: Int) }) (tail xs)
+getOpts conf ("--window":xs)    = getOpts (conf {
+                                    window = read (head xs) :: Int }) (tail xs)
+getOpts conf ("--move":xs)      = getOpts (conf {
+                                    move = (read (head xs) :: Int) }) (tail xs)
+getOpts _ _                     = Nothing
