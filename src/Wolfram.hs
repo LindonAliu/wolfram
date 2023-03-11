d5ea83d48ac0abc730982aaeea3472e930a525a6
Lindon ALIU <lindon.aliu@epitech.eu>
2023-03-11 19:12:53 +0100

    feat: add wolfram algorithm with all rules

--- src/Wolfram.hs
+++ src/Wolfram.hs
@@ -7,10 +7,30 @@
 
 module Wolfram (wolfram) where
 
+import Lib
 import Conf (Conf(..))
 
-makeFirstLine :: Int -> [Char]
-makeFirstLine n = take n (cycle " ") ++ "*" ++ take n (cycle " ")
+numberingSystem :: Int -> Char -> Char -> Char -> Char
+numberingSystem r '*' '*' '*' | ((r `div` 128) `mod` 2) == 1 = '*'
+numberingSystem r '*' '*' ' ' | ((r `div` 64) `mod` 2) == 1 = '*'
+numberingSystem r '*' ' ' '*' | ((r `div` 32) `mod` 2) == 1 = '*'
+numberingSystem r '*' ' ' ' ' | ((r `div` 16) `mod` 2) == 1 = '*'
+numberingSystem r ' ' '*' '*' | ((r `div` 8) `mod` 2) == 1 = '*'
+numberingSystem r ' ' '*' ' ' | ((r `div` 4) `mod` 2) == 1 = '*'
+numberingSystem r ' ' ' ' '*' | ((r `div` 2) `mod` 2) == 1 = '*'
+numberingSystem r ' ' ' ' ' ' | ((r `div` 1) `mod` 2) == 1 = '*'
+numberingSystem _ _ _ _ = ' '
 
-wolfram :: Conf -> IO ()
-wolfram conf = putStrLn (makeFirstLine (window conf `div` 2 - 1))
+applicateRule :: String -> Int -> String
+applicateRule (x:y:z:xs) r = numberingSystem r x y z : applicateRule (y:z:xs) r
+applicateRule (x:y:[]) r = numberingSystem r x y ' ' : applicateRule [y] r
+applicateRule (x:[]) r = numberingSystem r x ' ' ' ' : applicateRule [] r
+applicateRule [] _ = []
+
+wolfram :: Conf -> Int -> String -> IO ()
+wolfram (Conf _ _ 0 _ _) _ _ = return ()
+wolfram (Conf r 0 l w m) i str = printLine str i >>
+                                 wolfram (Conf r 0 (l - 1) w m) (i + 1)
+                                    (init (applicateRule ("  " ++ str ++ " ") r))
+wolfram (Conf r s l w m) i str = wolfram (Conf r (s - 1) l w m) (i + 1)
+                                    (init (applicateRule ("  " ++ str ++ " ") r))
